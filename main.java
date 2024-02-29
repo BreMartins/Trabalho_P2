107f1f6779a428a72f0a3594556ab9b3f48df6cf
123110236 SERGIO BRENNO MARTINS DE ALCANTARA PEREIRA <sergio.brenno.martins.alcantara.pereira@lcc3-036.lcc.ufcg.edu.br>
2024-02-29 10:34:29 -0300

    criei

---
+++ main.java
@@ -0,0 +1,7 @@
+/*                                                      Trabalho de P2      
+                                        Universidade Federal de Campina Grande
+Professor(a): Lívia Maria Rodrigues Sampaio Campos                       Data: 10/03/2024                                       
+integrantes: Sérgio Brenno Martins de Alcântara Pereira                  Matrícula: 123110236
+             Arthur Fernandes Vieira                                     Matrícula: 123110808
+             Adriano Porto ------                                        Matrícula: 12311----
+ */
