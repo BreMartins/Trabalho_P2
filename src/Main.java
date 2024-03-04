d0800255cc50f309cd6dcf6700514ed4b93ed151
Adriano Porto <adriano.porto.a.j@gmail.com>
2024-03-04 16:01:51 -0300

    feat: project structure

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+package src;
+
 /*                                                      Trabalho de P2      
                                         Universidade Federal de Campina Grande
 Professor(a): Lívia Maria Rodrigues Sampaio Campos                       Data: 10/03/2024                                       
 integrantes: Sérgio Brenno Martins de Alcântara Pereira                  Matrícula: 123110236
              Arthur Fernandes Vieira                                     Matrícula: 123110808
-             Adriano Porto ------                                        Matrícula: 12311----
+             Adriano Porto Andrade Júnior                                Matrícula: 123110597
  */
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
