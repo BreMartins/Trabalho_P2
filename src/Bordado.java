98e232aeeb1f3d921a3619cbdf74b4708d0f26de
Adriano Porto <adriano.porto.a.j@gmail.com>
2024-03-04 17:10:39 -0300

    feat: atualizar & imprimir & listar bordados

--- src/Bordado.java
+++ src/Bordado.java
@@ -5,16 +5,30 @@ public class Bordado {
     public int linhas;
     public int colunas;
 
-    public char[][] bordado;
+    public char[][] pontos;
+    public int numeroPontos;
 
     Bordado(int id, int linhas, int colunas) {
         this.id = id;
         this.linhas = linhas;
         this.colunas = colunas;
-        this.bordado = new char[linhas][colunas];
+        this.pontos = new char[linhas][colunas];
+        this.numeroPontos = 0;        
+
+        // Inciializa os pontos do bordado com espaços 
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                this.pontos[i][j] = ' ';
+            }
+        }
     }
 
     public void adicionarPonto(int linha, int coluna, char ponto) {
-        this.bordado[linha][coluna] = ponto;
+        if (ponto == ' ') {
+            this.numeroPontos--;
+        } else if (this.pontos[linha][coluna] == ' ') {
+            this.numeroPontos++;
+        }
+        this.pontos[linha][coluna] = ponto;
     }
 }
--- src/MenuBordados.java
+++ src/MenuBordados.java
@@ -7,8 +7,80 @@ public class MenuBordados {
         this.bordados = new Bordado[10];
     }
 
-    public void registrarBordado(int id, int linhas, int colunas) {
+    public void cadastrarBordado(int id, int linhas, int colunas) throws Error{
+
+        if (id < 0 || id > 9) {
+            throw new Error();
+        }
+
+        if (linhas < 1 || colunas < 1 || linhas > 100 || colunas > 100) {
+            throw new Error();
+        }
+
         Bordado bordado = new Bordado(id, linhas, colunas);
         bordados[id] = bordado;
+    }
+
+    public void atualizarBordado(int id, int bordadoLinha, int bordadoColuna, char ponto) {
+
+        Bordado bordado = this.bordados[id];
+        
+        if (bordado == null) {
+            throw new Error();
+        }
+
+        int cols = bordado.colunas;
+        int rows = bordado.linhas;
+
+
+        if (bordadoLinha < 0 || bordadoLinha >= rows || bordadoColuna < 0 || bordadoColuna >= cols) {
+            throw new Error();
+        }
+
+        if (ponto != '/' && ponto != '\\' && ponto != '|' && ponto != '-' && ponto != 'x' && ponto != ' ') {
+            throw new Error();
+        }
+
+        this.bordados[id].adicionarPonto(bordadoLinha, bordadoColuna, ponto);
+    }
+
+    public void imprimirBordado(int id) throws Error {
+        Bordado bordado = this.bordados[id];
+        
+        if (bordado == null) {
+            throw new Error();
+        }
+
+        int cols = bordado.colunas;
+        int rows = bordado.linhas;
+
+        System.out.print('|');
+        for (int i = 0; i < cols; i++) {
+            System.out.print('-');
+        }
+        System.out.println('|');
+
+        for (int i = 0; i < cols; i++) {
+            System.out.print('|');
+
+            for (int j = 0; j < rows; j++) {
+                System.out.print(bordado.pontos[i][j]);
+            }
+
+            System.out.println('|');
+        }
+    }
+    
+    public void ListarBordados() {
+        for (int i = 0; i < 10; i++) {
+            if (this.bordados[i] != null ) {
+                System.out.println(
+                    i + " - " +
+                    this.bordados[i].linhas  + " x " + 
+                    this.bordados[i].colunas + " - " +
+                    this.bordados[i].numeroPontos + " pontos"
+                );
+            }
+        }
     }
 }
