d6179a20e5eddfe26cb881869421214e075ebad7
Adriano Porto <adriano.porto.a.j@gmail.com>
2024-03-04 16:22:40 -0300

    feat: registrar bordado

--- src/Bordado.java
+++ src/Bordado.java
@@ -1,5 +1,20 @@
 package src;
 
 public class Bordado {
-    
+    public int id;
+    public int linhas;
+    public int colunas;
+
+    public char[][] bordado;
+
+    Bordado(int id, int linhas, int colunas) {
+        this.id = id;
+        this.linhas = linhas;
+        this.colunas = colunas;
+        this.bordado = new char[linhas][colunas];
+    }
+
+    public void adicionarPonto(int linha, int coluna, char ponto) {
+        this.bordado[linha][coluna] = ponto;
+    }
 }
--- src/MenuBordados.java
+++ src/MenuBordados.java
@@ -1,5 +1,14 @@
 package src;
 
 public class MenuBordados {
-    
+    Bordado[] bordados;
+
+    MenuBordados() {
+        this.bordados = new Bordado[10];
+    }
+
+    public void registrarBordado(int id, int linhas, int colunas) {
+        Bordado bordado = new Bordado(id, linhas, colunas);
+        bordados[id] = bordado;
+    }
 }
